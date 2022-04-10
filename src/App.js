710efcb0a1e843f9922852b95daa6608cf1868ce
Junith-K <88233179+Junith-K@users.noreply.github.com>
2022-04-11 01:01:56 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import Counter from "./components/Counter";
 import Value from "./components/Value";
 
 function App() {
+  const count = 5;
+
   return (
     <div className="App">
       <Value></Value>
-      <Counter></Counter>
+      <Counter count={count}></Counter>
     </div>
   );
 }
