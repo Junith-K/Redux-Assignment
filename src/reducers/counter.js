4b707f10627a07a912f19925772633ddb16f7484
Junith-K <88233179+Junith-K@users.noreply.github.com>
2022-04-11 01:03:01 +0530

    Update counter.js

--- src/reducers/counter.js
+++ src/reducers/counter.js
@@ -1,7 +1,8 @@
 const counterReducer = (state = 0, action) => {
   switch (action.type) {
     case "INCREMENT":
-      return state + 1;
+      if (state === 0) return (state = action.payload);
+      else return state + 1;
     default:
       return state;
   }
